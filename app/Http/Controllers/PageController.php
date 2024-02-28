8d5630fae645a01d7138cf8d234e1019f517e279
AfrizalDS <angelicacoustarika@gmail.com>
2024-02-28 16:02:28 +0700

    Add and modify controller (praktikum)

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() { 
+        return 'Selamat Datang'; 
+    }
+    
+    public function about() { 
+        return '2241720122 <br> Afrizal Dwi Septian'; 
+    } 
+
+    public function articles($id)
+    {
+        return 'Halaman dengan ID: ' . $id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,13 +33,13 @@ Route::get('/world', function () {
 return 'World'; 
 }); 
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return '2241720122 <br> Afrizal Dwi Septian';
-});
+// Route::get('/about', function () {
+//     return '2241720122 <br> Afrizal Dwi Septian';
+// });
 
 Route::get('/user/{name}', function ($name) { 
     return 'Nama saya '.$name; 
@@ -43,10 +50,32 @@ Route::get('/posts/{post}/comments/{comment}', function
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
     }); 
 
-Route::get('/articles/{id}', function ($id) { 
-    return 'Halaman Dengan Id:'.$id; 
-    });
+// Route::get('/articles/{id}', function ($id) { 
+//     return 'Halaman Dengan Id:'.$id; 
+//     });
     
 Route::get('/user/{name?}', function ($name = null) { 
     return 'Nama saya '.$name; 
     });
+
+ Route::get('/hello', [WelcomeController::class,'hello']); 
+ 
+ 
+ Route::get('/index', [PageController::class,'index']); 
+ Route::get('/about', [PageController::class,'about']); 
+ Route::get('/articles', [PageController::class,'articles']); 
+
+ 
+ Route::get('/index', [HomeController::class,'index']); 
+ Route::get('/about', [AboutController::class,'about']); 
+ Route::get('/articles/{id}', [ArticleController::class,'articles']); 
+
+//  Route::resource('photos', PhotoController::class); 
+ 
+ Route::resource('photos', PhotoController::class)->only([ 
+    'index', 'show' 
+ ]); 
+ 
+Route::resource('photos', PhotoController::class)->except([ 
+'create', 'store', 'update', 'destroy' 
+]);
